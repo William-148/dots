8125d77d6c4eb6391b5f6e5a2eeec00b1957faee
William Cán <wilenrike@gmail.com>
2025-04-18 16:18:22 -0600

    update: Hyprland dots

--- hypr/scripts/xdg.sh
+++ hypr/scripts/xdg.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 sleep 1
-
-# kill all posible running xdg-desktop-portals
 killall -e xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
-
-# start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
-
-# start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
-sleep 1
